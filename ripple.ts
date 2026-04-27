1325a08f6d52c95ff9967973bfa2b18eaa55b80c
asplunds <71442467+asplunds@users.noreply.github.com>
2026-04-27 10:25:36 +0200

    fix: logic problem

--- ripple.ts
+++ ripple.ts
@@ -256,6 +256,6 @@ function isTouchDevice(): boolean {
   return (
     "ontouchstart" in window ||
     navigator.maxTouchPoints > 0 ||
-    ((navigator as any)?.msMaxTouchPoints ?? 0 > 0)
+    (((navigator as any)?.msMaxTouchPoints ?? 0) > 0)
   );
 }
